a831d78aff4e048ddac6bbfb71a03408aa3de19f
Clement Udensi <cklemmzy@gmail.com>
2016-11-19 19:41:59 +0000

    This are untracked files I had to coomit in other to revert

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  # get 'employees/first_name:string'
+
+  # get 'employees/last_name:string'
+
+  # get 'employees/middle_name:string'
+
+  # get 'employees/gender:string'
+
+  # get 'employees/date_of_birth:date'
+
+  # get 'employees/employee_id:string'
+
+  # get 'employees/bank_account_id:integers'
+
+  # get 'employees/job_title:string'
+
+  # get 'employees/organizational_id:integers'
+
+  # get 'employees/date_joined_company:date'
+
+  # get 'employees/date_left_company:date'
+
+  # get 'employees/residential_address:text'
+  resources :employees
   # resources :admin
   # resources :welcome
   get 'admin/dashboard'
